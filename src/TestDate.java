062a6077fc3ffedc510863e5e0eac43fc7cec5fa
heidsoft <heidsoft@qq.com>
2014-08-28 15:02:57 +0800

    时间处理

---
+++ src/TestDate.java
@@ -0,0 +1,21 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 时间处理比较
+ * @author heidsoft
+ *
+ */
+public class TestDate {
+	
+	public static void main(String[] args){
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date1 = new Date((long)1407902056 * (long)1000);
+        Date date2 = new Date((long)1407981217 * (long)1000);
+        Date date3 = new Date((long)1409026689 * (long)1000);
+         
+        System.out.println( formatter.format(date1));
+        System.out.println( formatter.format(date2));
+        System.out.println( formatter.format(date3));
+	}
+}
