e67b973f1646805a66752717f4be26888a09ea6a
heidsoft <heidsoft@qq.com>
2017-12-24 22:52:10 +0800

    Update 测试线程

---
+++ src/main/java/com/heidsoft/thread/lamba/LambaTaskThreadDeme.java
@@ -0,0 +1,29 @@
+package com.heidsoft.thread.lamba;
+
+import java.util.concurrent.TimeUnit;
+
+public class LambaTaskThreadDeme {
+
+    public static void  main(String[] args){
+
+        Runnable task = () ->{
+            String threadName = Thread.currentThread().getName();
+            System.out.println("hello 1 " + threadName);
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("hello 2 " + threadName);
+        };
+
+        task.run();
+
+        Thread mythread = new Thread(task);
+
+        mythread.start();
+
+        System.out.println("Done");
+    }
+}
