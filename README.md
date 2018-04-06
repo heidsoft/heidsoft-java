bca512c11dcdff3b24591e55f30e233a60e11e1b
Bin Liu <bin.liu@magicwindow.cn>
2018-04-06 17:00:53 +0800

    Update 测试非线程安全

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 
 ## Java jvm
 
+JVM性能调优监控工具专题一
+http://josh-persistence.iteye.com/blog/2161848
+
+
 ## Java nio
 
 ## Java thread
---
+++ heidsoft-java-thread/src/main/java/Deadlock.java
@@ -0,0 +1,65 @@
+/**
+ *
+ *<p>Java stack information for the threads listed above:
+ ===================================================
+ "t2":
+ at Deadlock$2.run(Deadlock.java:32)
+ - waiting to lock <0x00000007955fcb10> (a java.lang.Object)
+ - locked <0x00000007955fcb20> (a java.lang.Object)
+ "t1":
+ at Deadlock$1.run(Deadlock.java:16)
+ - waiting to lock <0x00000007955fcb20> (a java.lang.Object)
+ - locked <0x00000007955fcb10> (a java.lang.Object)
+
+ Found 1 deadlock.</p>
+ * @description 死锁模拟
+ * @author heidsoft
+ */
+public class Deadlock {
+    public static void main(String[] args) {
+        System.out.println(" 死锁模拟 start ----- ");
+        final Object lockObj1 = new Object(), lockObj2 = new Object();
+
+        Thread t1 = new Thread("t1") {
+            @Override
+            public void run() {
+                synchronized (lockObj1) {
+                    try {
+                        System.out.println("thread t1 start...");
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (lockObj2) {
+                        System.out.println("thread t1 done....");
+                    }
+                }
+            }
+        };
+
+        Thread t2 = new Thread("t2") {
+            @Override
+            public void run() {
+                synchronized (lockObj2) {
+                    try {
+                        System.out.println("thread t2 start...");
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (lockObj1) {
+                        System.out.println("thread t2 done...");
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+
+}
+
+
