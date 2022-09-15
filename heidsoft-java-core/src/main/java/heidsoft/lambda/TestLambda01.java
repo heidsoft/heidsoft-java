dddb946343fb5b6176f587cc58aa66b2ecc82994
jake.liu <jake.liu@baozun.com>
2022-09-15 23:54:42 +0800

    采用jdk17 测试lambda案例

---
+++ heidsoft-java-core/src/main/java/heidsoft/lambda/TestLambda01.java
@@ -0,0 +1,15 @@
+package heidsoft.lambda;
+
+import java.util.Arrays;
+
+/**
+ * 函数式接口，lambda测试
+ */
+public class TestLambda01 {
+    public static void main(String[] args) {
+        var books = new String[]{"java", "python", "golang", "c++"};
+        //int test =   (String first,String second)->first.length()-second.length()
+        Arrays.sort(books, (String first, String second) -> first.length() - second.length());
+        System.out.println(Arrays.toString(books));
+    }
+}
